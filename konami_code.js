04f3496790f7684e6f305887ee2c27a00ba013e5
Flatiron School <ellis.paige.v@gmail.com>
2016-10-19 14:39:09 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
+init()
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', konami)
+}
+
+
+function konami(e) {
+  var key = parseInt(e.which || e.detail)
+  console.log(key)
+  if (key === code[index]) {
+    index ++
+  }
+
+  if (index === code.length) {
+    alert("Surprise!")
+    index = 0
+    document.body.removeEventListener('keydown')
+  }
 }
